33319f543f013bd27a7d14ca4bd278f89ad24ec6
Ben Cavens <cavensben@gmail.com>
2016-05-12 23:58:04 +0200

    set in memory db for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,23 @@ namespace Thinktomorrow\Repo\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-class TestCase extends OrchestraTestCase{}
+class TestCase extends OrchestraTestCase{
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+}
